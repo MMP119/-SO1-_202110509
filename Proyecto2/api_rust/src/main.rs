f01deafcf274687320b9ed7eaa81c8cbf796150a
MMP119 <mariomarroquin1905@gmail.com>
2025-04-06 00:33:03 -0600

    fix: :bug: Actualización en api de rust
    Se ha corregido la api en rust para la conexión con la de go para que fucnione grafana

--- Proyecto2/api_rust/src/main.rs
+++ Proyecto2/api_rust/src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
+use reqwest::Client;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct WeatherInput {
     description: String,
     country: String,
@@ -9,16 +10,35 @@ struct WeatherInput {
 }
 
 async fn input_handler(item: web::Json<WeatherInput>) -> impl Responder {
-    println!("input: {:?}", item);
-    HttpResponse::Ok().json(item.into_inner())
+    println!("input recibido en Rust: {:?}", item);
+
+    //reenviar al servicio en Go
+    let client = Client::new();
+    let result = client
+        .post("http://api-http-service:8081/input") // nombre del service de Go
+        .json(&*item)
+        .send()
+        .await;
+
+    match result {
+        Ok(resp) => {
+            println!("Respuesta de Go API: {:?}", resp.status());
+            HttpResponse::Ok().body("Reenviado correctamente al servicio Go")
+        }
+        Err(err) => {
+            eprintln!("Error al reenviar: {:?}", err);
+            HttpResponse::InternalServerError().body("Error al reenviar al servicio Go")
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("iniciando en http://0.0.0.0:8080");
+    println!("API Rust escuchando en http://0.0.0.0:8080");
+
     HttpServer::new(|| {
         App::new()
-            .route("/input", web::post().to(input_handler)) // se define la ruta /input
+            .route("/input", web::post().to(input_handler))
     })
     .bind("0.0.0.0:8080")?
     .run()
