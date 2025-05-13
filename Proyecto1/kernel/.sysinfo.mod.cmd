127fcdd64a2024babda87ab3e643bbcf880e82b2
Mario <mariomarroquin1905@gmail.com>
2025-05-13 11:05:57 -0600

    refactor: :memo: Eliminacion de codigo basura

--- Proyecto1/kernel/.sysinfo.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/mario/Escritorio/GitHub/-SO1-_202110509/Proyecto1/kernel/sysinfo.mod := printf '%s\n'   sysinfo.o | awk '!x[$$0]++ { print("/home/mario/Escritorio/GitHub/-SO1-_202110509/Proyecto1/kernel/"$$0) }' > /home/mario/Escritorio/GitHub/-SO1-_202110509/Proyecto1/kernel/sysinfo.mod
--- Proyecto1/kernel/sysinfo.mod
+++
@@ -1,1 +0,0 @@
-/home/mario/Escritorio/GitHub/-SO1-_202110509/Proyecto1/kernel/sysinfo.o
